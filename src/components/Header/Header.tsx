27c4ea3e1a753b52ff10a693757f067e9158a8e7
Jonathan Marsh <hello@jonmarsh.dev>
2024-02-29 23:45:11 +0000

    feat: first-draft responsive header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,37 @@
-import { NavLink } from 'react-router-dom'
-import * as styles from "./Header.module.css"
 import React from 'react'
 
+import { NavLink } from 'react-router-dom'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RiCloseLine } from "react-icons/ri";
+
+import * as styles from "./Header.module.css"
+
 export function Header() {
-  return <nav className={styles.header}>
-    <NavLink to="/home"><p>Home</p></NavLink>
-    <NavLink to="/story"><p>Our Story</p></NavLink>
-    <NavLink to="/home"><h1>Jon & Beth</h1></NavLink>
-    <NavLink to="/travel"><p>Travel & Accommodation</p></NavLink>
-    <NavLink to="/faq"><p>FAQ</p></NavLink>
-  </nav>
+  return (
+    <div className={styles.header}>
+      <div className={styles.desktop}>
+        <nav>
+          <NavLink to="/home"><p>Home</p></NavLink>
+          <NavLink to="/story"><p>Our Story</p></NavLink>
+          <NavLink to="/home"><h1>Jon & Beth</h1></NavLink>
+          <NavLink to="/travel"><p>Travel & Accommodation</p></NavLink>
+          <NavLink to="/faq"><p>FAQ</p></NavLink>
+        </nav>
+      </div>
+      <div className={styles.mobile}>
+        <label>
+          <input type="checkbox" />
+          <RiCloseLine size={32} />
+          <RxHamburgerMenu size={32} />
+          <h1>Jon & Beth</h1>
+        </label>
+        <nav>
+          <NavLink to="/home"><p>Home</p></NavLink>
+          <NavLink to="/story"><p>Our Story</p></NavLink>
+          <NavLink to="/travel"><p>Travel & Accommodation</p></NavLink>
+          <NavLink to="/faq"><p>FAQ</p></NavLink>
+        </nav>
+      </div>
+    </div>
+  )
 }
