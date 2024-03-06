5cdc82d1400015217f49aead94f7a65f7484bd72
Jonathan Marsh <hello@jonmarsh.dev>
2024-03-06 11:19:10 +0000

    fix: init GA on component load

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,14 +7,17 @@ import { ErrorBoundary } from "../ErrorBoundary/ErrorBoundary"
 import * as styles from "./Layout.module.css"
 
 const GOOGLE_ANALYTICS_KEY = import.meta.env.VITE_GOOGLE_ANALYTICS_KEY
-ReactGA.initialize([{ trackingId: GOOGLE_ANALYTICS_KEY }])
 
 export function Layout() {
-  const location = useLocation()
+  const { pathname } = useLocation()
 
   React.useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: location.pathname, title: location });
-  }, [location])
+    ReactGA.initialize([{ trackingId: GOOGLE_ANALYTICS_KEY }])
+  }, [])
+
+  React.useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: pathname, title: pathname });
+  }, [pathname])
 
   return <div className={styles.layout}>
     <Header />
