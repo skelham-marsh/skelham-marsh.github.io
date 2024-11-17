e75923e589502682031ddb5a6e2ef105896d0659
Jonathan Marsh <hello@jonmarsh.dev>
2024-11-17 16:58:16 +0000

    fix: rsvp sizing

--- src/pages/RSVP/RSVP.tsx
+++ src/pages/RSVP/RSVP.tsx
@@ -3,6 +3,6 @@ import styles from "./RSVP.module.css"
 
 export function RSVP() {
   return <div className={styles.wrapper}>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfUkwU_9_MYcJbpvOgI46xYqvreZewEGM2WCYldz1pKH3aUHg/viewform?embedded=true" width="640" height="1237" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfUkwU_9_MYcJbpvOgI46xYqvreZewEGM2WCYldz1pKH3aUHg/viewform?embedded=true" >Loading…</iframe>
   </div>
 }
